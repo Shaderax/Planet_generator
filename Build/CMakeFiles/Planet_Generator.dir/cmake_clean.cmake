69b9b87febe4386309a530257b0c79ddcd054864
Shaderax <rouzeval1998@live.fr>
2020-01-09 18:08:40 +0100

    Basic Window

---
+++ Build/CMakeFiles/Planet_Generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Planet_Generator.dir/Sources/main.cpp.o"
+  "Planet_Generator.pdb"
+  "Planet_Generator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Planet_Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,31 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 int main()
 {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//printf("%s\n", glGetString(GL_VERSION));
+
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 	return (0);
 }
